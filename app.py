a0c7fbc6334fff87f0a71c430ef24b933c619d2b
Alper Akgun <alper.akgun@sayarus.com>
2023-08-25 11:50:24 +0300

    Configuration options

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import logging
 import os
 
 app = Flask(__name__)
-app.config['models_folder'] = os.environ.get('KODLOKAL_MODELS') or './models/'
-app.config['use_model'] = os.environ.get('KODLOKAL_SIZE') or 'xs'
+app.config.from_pyfile('config.py')
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -14,16 +13,15 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
-if app.config['use_model'] == 'm':
-  print("Using M model")
-  code_model_name = f"{app.config['models_folder']}WizardCoder-15B-1.0.ggmlv3.q5_1.bin"
-  code_model_type="starcoder"
-else:
-  print("Using XS model")
-  code_model_name = f"{app.config['models_folder']}stablecode-completion-alpha-3b-4k.ggmlv1.q5_1.bin"
-  code_model_type = "gpt-neox"
-  text_model_name = f"{app.config['models_folder']}orca_mini_v3_7b.ggmlv3.q5_0.bin"
-  text_model_type = "llama"
+code_model_name = f"{app.config['MODELS_FOLDER']}{app.config['CODE_MODEL']}"
+code_model_type = app.config['CODE_MODEL_TYPE']
+text_model_name = f"{app.config['MODELS_FOLDER']}{app.config['TEXT_MODEL']}"
+text_model_type = app.config['TEXT_MODEL_TYPE']
+
+print(f"""Using models
+text: {text_model_name}/{text_model_type}
+code: {code_model_name}/{code_model_type}"
+""")
 
 code_model = AutoModelForCausalLM.from_pretrained(code_model_name, model_type=code_model_type)
 text_model = AutoModelForCausalLM.from_pretrained(text_model_name, model_type=text_model_type)
