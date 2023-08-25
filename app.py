fdd8bf6087eb505c14dbf54c82cd28f3859e91ba
Alper Akgun <alper.akgun@sayarus.com>
2023-08-25 11:20:57 +0300

    Text completion endpoint

--- app.py
+++ app.py
@@ -16,17 +16,23 @@ log = logging.getLogger(__name__)
 
 if app.config['use_model'] == 'm':
   print("Using M model")
-  model_name = f"{app.config['models_folder']}WizardCoder-15B-1.0.ggmlv3.q5_1.bin"
-  model_type="starcoder"
+  code_model_name = f"{app.config['models_folder']}WizardCoder-15B-1.0.ggmlv3.q5_1.bin"
+  code_model_type="starcoder"
 else:
   print("Using XS model")
-  model_name = f"{app.config['models_folder']}stablecode-completion-alpha-3b-4k.ggmlv1.q5_1.bin"
-  model_type = "gpt-neox"
+  code_model_name = f"{app.config['models_folder']}stablecode-completion-alpha-3b-4k.ggmlv1.q5_1.bin"
+  code_model_type = "gpt-neox"
+  text_model_name = f"{app.config['models_folder']}orca_mini_v3_7b.ggmlv3.q5_0.bin"
+  text_model_type = "llama"
 
-model = AutoModelForCausalLM.from_pretrained(model_name,
-                                             model_type=model_type)
-def suggest(query):
-  return model(query, temperature=0.2, max_new_tokens=42)
+code_model = AutoModelForCausalLM.from_pretrained(code_model_name, model_type=code_model_type)
+text_model = AutoModelForCausalLM.from_pretrained(text_model_name, model_type=text_model_type)
+
+def code_suggest(query):
+  return code_model(query, temperature=0.2, max_new_tokens=42)
+
+def text_suggest(query):
+  return text_model(query, temperature=0.2, max_new_tokens=42)
 
 @app.route("/")
 def main():
@@ -40,7 +46,35 @@ def code_completions():
   if query is None or len(query) <= 3:
     return nil
   else:
-    result = suggest(query)
+    result = code_suggest(query)
+    log.info(f"/Finished Result={result}")
+    if result is not None:
+      return result
+    else:
+      return nil
+
+def text_prompt(query, input=""):
+  return f"""### System:
+You are an AI assistant that follows instruction extremely well. Help as much as you can.
+
+### User:
+{query}
+
+### Input:
+{input}
+
+### Response:"""
+
+@app.route("/text/completions", methods=["POST"])
+def text_completions():
+  data = request.json
+  query = data.get("q")
+  prompt = text_prompt(query)
+  log.info(f"Starting Query={query}")
+  if query is None or len(query) <= 3:
+    return nil
+  else:
+    result = text_suggest(query)
     log.info(f"/Finished Result={result}")
     if result is not None:
       return result
